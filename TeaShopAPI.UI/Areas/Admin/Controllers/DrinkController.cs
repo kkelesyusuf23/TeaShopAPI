3d35f8f4c01ae366face0b647ee28eb0a3157f44
kkelesyusuf <kkelesyusuf23@gmail.com>
2024-01-10 22:33:50 +0300

    drink listeleme consume edildi

--- TeaShopAPI.UI/Areas/Admin/Controllers/DrinkController.cs
+++ TeaShopAPI.UI/Areas/Admin/Controllers/DrinkController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TeaShopAPI.UI.Dtos.DrinkDtos;
 
 namespace TeaShopAPI.UI.Areas.Admin.Controllers
 {
 	[Area("Admin")]
 	public class DrinkController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _httpClientfactory;
+
+		public DrinkController(IHttpClientFactory httpClientfactory)
 		{
+			_httpClientfactory = httpClientfactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientfactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7272/api/Drinks");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultDrinkDto>>(jsonData);
+				return View(values);
+			}
 			return View();
 		}
 	}
---
+++ TeaShopAPI.UI/Dtos/DrinkDtos/ResultDrinkDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TeaShopAPI.UI.Dtos.DrinkDtos
+{
+	public class ResultDrinkDto
+	{
+			public int drinkID { get; set; }
+			public string drinkName { get; set; }
+			public decimal drinkPrice { get; set; }
+			public string drinkImageURL { get; set; }
+
+	}
+}
